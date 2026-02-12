15d044b42407d4b898c03528e609aca9f6cc9a9b
hyeonmin02 <hyenmin5@gmail.com>
2026-02-13 02:58:19 +0900

    Lv6 비밀번호 암호화 의존성 추가 회원가입encode로그인matches 추가

--- src/main/java/com/schedule2/user/service/UserService.java
+++ src/main/java/com/schedule2/user/service/UserService.java
@@ -1,9 +1,9 @@
 package com.schedule2.user.service;
 
+import com.schedule2.config.PasswordEncoder;
 import com.schedule2.user.dto.*;
 import com.schedule2.user.entity.User;
 import com.schedule2.user.repository.UserRepository;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,14 +15,15 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public RegisterResponse register(RegisterRequest request) {
-
+        String encodePassword = passwordEncoder.encode(request.getPassword());
         User user = new User(
                 request.getUserName()
-                , request.getEmail()
-                , request.getPassword());
+                , request.getEmail(),
+                encodePassword);
 
         User savedUser = userRepository.save(user);
 
@@ -84,14 +85,15 @@ public class UserService {
 
     }
 
-    public User login(@Valid LoginUserRequest request) {
+    public User login(LoginUserRequest request) {
         // 사용자가 요청한 이메일을 가지고 DB에서 이메일이 같은 유저를 찾아서 없으면 예외를 던지고 있으면 user변수에 담아라
         User user = userRepository.findByEmail(request.getEmail()).orElseThrow(
                 () -> new IllegalStateException("이메일 또는 비밀번호가 올바르지 않습니다!"));
 
         // DB에서 가져온 유저의 비밀번호와 요청한 패스워드가 일치하지않는가?
-        if (!user.getPassword().equals(request.getPassword())) {
-            throw new IllegalStateException("비밀번호가 일치하지 않습니다!");
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword()
+        )) {
+            throw new IllegalStateException("이메일 또는 비밀번호가 일치하지 않습니다!");
         }
         return user;
     }
