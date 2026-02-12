4b2e764e83d2f84482f4a90d48c40d0d442ea30c
hyeonmin02 <hyenmin5@gmail.com>
2026-02-12 19:29:56 +0900

    Lv 3 회원가입 password size assign

--- src/main/java/com/schedule2/user/dto/CreateUserRequest.java
+++ src/main/java/com/schedule2/user/dto/CreateUserRequest.java
@@ -1,12 +1,18 @@
 package com.schedule2.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class CreateUserRequest {
+    @NotBlank
+    @Size(max = 4)
     private String userName;
+    @NotBlank
     private String email;
+    @NotBlank
+    @Size(min = 8, message = "비밀번호는 8자 이상입니다.")
     private String password;
 
 }
--- src/main/java/com/schedule2/user/dto/UpdateUserRequest.java
+++ src/main/java/com/schedule2/user/dto/UpdateUserRequest.java
@@ -1,10 +1,19 @@
 package com.schedule2.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UpdateUserRequest {
+    @NotBlank
+    @Size(max = 4)
     private String userName;
+    @NotBlank
+    @Email
     private String Email;
+    @NotBlank
+    @Size(min = 8)
     private String password;
 }
