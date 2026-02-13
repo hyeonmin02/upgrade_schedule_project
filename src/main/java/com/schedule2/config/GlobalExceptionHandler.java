0891893836450c57b055057d37204a744d4690f7
hyeonmin02 <hyenmin5@gmail.com>
2026-02-13 14:08:18 +0900

    dto 선언된 validation 검증 전역예외처리 추가

--- src/main/java/com/schedule2/config/GlobalExceptionHandler.java
+++ src/main/java/com/schedule2/config/GlobalExceptionHandler.java
@@ -3,14 +3,20 @@ package com.schedule2.config;
 import com.schedule2.exception.ServiceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(IllegalStateException.class)
-    public ResponseEntity<String> handleIllegalStateException(IllegalStateException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("요청 오류: " + e.getMessage());
+
+    // DTO에 선언된 @NotBlank, @Size 등 Validation 검증이 실패하면 Spring이 알아서 MethodArgumentNotValidException 자동 발생
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidation(MethodArgumentNotValidException e) {
+
+                        //검증 결과 모음상자.실패한필드가져와.DTO에 적어둔 에러메시지값가져와 담음
+        String message = e.getBindingResult().getFieldError().getDefaultMessage();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
     }
 
     @ExceptionHandler(ServiceException.class)
